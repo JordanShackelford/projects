3083d3b69d6acaab71d002c74a05b3793f47f2aa
Jordan <jordanshackelford2@gmail.com>
2021-10-27 14:03:30 -0500

    created tdd-counter project and installed jest and enzyme for testing

--- react-googlesheets/src/App.js
+++ react-googlesheets/src/App.js
@@ -1,68 +1,53 @@
-import React, { useState } from 'react';
-import { Button, Form, Container, Header } from 'semantic-ui-react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
 
-function App() {
-	const [name, setName] = useState('');
-	const [age, setAge] = useState('');
-	const [salary, setSalary] = useState('');
-	const [hobby, setHobby] = useState('');
+export default class App extends Component {
+  constructor(props) {
+    super(props)
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    this.state = {
+       name: '',
+       age: '',
+       salary: '',
+       hobby: ''
+    }
+  }
 
-		const objt = { name, age, salary, hobby };
+  changeHandler = (e) => {
+    this.setState({[http://e.target.name] : http://e.target.value})}
 
-		axios
-			.post(
-				'https://sheet.best/api/sheets/71b175ce-7072-4081-bc7c-115babccc89a',
-				objt
-			)
-			.then((response) => {
-				console.log(response);
-			});
-	};
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  },
 
-	return (
-		<Container fluid className="container">
-			<Header as="h2">React google sheet</Header>
-			<Form className="form">
-				<Form.Field>
-					<label>Name</label>
-					<input
-						placeholder="Enter your Name"
-						onChange={(e) => setName(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Age</label>
-					<input
-						placeholder="Enter your Age"
-						onChange={(e) => setAge(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Salary</label>
-					<input
-						placeholder="Enter your Salary"
-						onChange={(e) => setSalary(e.target.value)}
-					/>
-				</Form.Field>
-				<Form.Field>
-					<label>Hobby</label>
-					<input
-						placeholder="Enter your Hobby"
-						onChange={(e) => setHobby(e.target.value)}
-					/>
-				</Form.Field>
+  render() {
+    const { name, age, salary, hobby } = this.state;    (*)
+    return (
+      <Container fluid className="container">
+        <Header as='h2'>React Google Sheets!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+          </Form.Field>
 
-				<Button color="blue" type="submit" onClick={handleSubmit}>
-					Submit
-				</Button>
-			</Form>
-		</Container>
-	);
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
 }
-
-export default App;
