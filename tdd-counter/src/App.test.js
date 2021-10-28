9d06bf8ca3230ae91e10bfbe46534c29377b1fb8
Jordan <jordanshackelford2@gmail.com>
2021-10-28 07:18:06 -0500

    added working test for to find text of 'this is counter app' in document, and the test is passed

--- tdd-counter/src/App.test.js
+++ tdd-counter/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('counter testing', () => {
+  test("render the title of counter", () => {
+    const {getByText} = render(<App />);
+    const linkElement = getByText('This is counter app');
+    expect(linkElement).toBeInTheDocument();
+  });
+})
