a3ca676c2d6e625c7c919743751e5c61ce0a6557
Jordan <jordanshackelford2@gmail.com>
2021-10-28 08:00:57 -0500

    added test and passes test for click event of button incrementing counter

--- tdd-counter/src/App.test.js
+++ tdd-counter/src/App.test.js
@@ -7,8 +7,26 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('counter testing', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  });
+
   test("render the title of counter", () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('h1').text()).toContain('This is counter app');
   });
-})
+
+  test("render a button with text of 'increment'", () => {
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("render the click event of increment button and increment counter value", () => {
+    wrapper.find("#increment-btn").simulate('click');
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  });
+});
