232ed3e591185f347746f3fa57fbf24ad0447363
Jordan <jordanshackelford2@gmail.com>
2021-10-28 07:26:37 -0500

    now tests for specifically an h1 tag containing our text

--- tdd-counter/src/App.test.js
+++ tdd-counter/src/App.test.js
@@ -4,13 +4,11 @@ import App from "./App";
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 configure({ adapter: new Adapter() });
 
 describe('counter testing', () => {
   test("render the title of counter", () => {
-    const {getByText} = render(<App />);
-    const linkElement = getByText('This is counter app');
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain('This is counter app');
   });
 })
