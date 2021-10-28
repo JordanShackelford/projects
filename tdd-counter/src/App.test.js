373cf87bff43c6015169881a06fefcbb8ea349b5
Jordan <jordanshackelford2@gmail.com>
2021-10-28 07:21:26 -0500

    imported enzyme

--- tdd-counter/src/App.test.js
+++ tdd-counter/src/App.test.js
@@ -2,6 +2,11 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 describe('counter testing', () => {
   test("render the title of counter", () => {
     const {getByText} = render(<App />);
