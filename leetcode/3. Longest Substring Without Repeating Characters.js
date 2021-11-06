d5cf37cd46b3493a89a7b9512a263992ffc1ec74
Jordan <jordanshackelford2@gmail.com>
2021-11-06 10:15:33 -0500

    leetcode #3 current solution, not solved

--- leetcode/3. Longest Substring Without Repeating Characters.js
+++ leetcode/3. Longest Substring Without Repeating Characters.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
  var lengthOfLongestSubstring = function(s) {
-    
+    s = s.split("");
+    var startingIndex = 0;
+    var endingIndex = 1;
+    var strings = [];
+    var currentStr = "";
+    for(var i = 0; i < s.length; i++){
+        //check if the next character is included in our current string
+        if(currentStr.includes(s[endingIndex])){
+            strings += s.join(startingIndex, endingIndex);
+            startingIndex = endingIndex;
+        } else {
+            endingIndex += 1;
+        }
+    }
+    return strings;
 };
