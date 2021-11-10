c6440f1f6afd79c03d6682e7750a4ac0aab78971
Jordan <jordanshackelford2@gmail.com>
2021-11-09 20:32:12 -0600

    fixed PokemanCard import

--- sveltekit-pokedex/src/routes/index.svelte
+++ sveltekit-pokedex/src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
     import {pokemon} from "../stores/pokestore";
-    import PokemanCard from "src/components/pokemanCard.svelte";
+    import PokemanCard from "../components/pokemanCard.svelte";
 </script>
 
 <svelte:head>
@@ -8,6 +8,8 @@
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-{#each $pokemon as pokeman}
-    <PokemanCard pokeman={pokeman}/>
-{/each}
+<div clas="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each $pokemon as pokeman}
+        <PokemanCard pokeman={pokeman}/>
+    {/each}
+</div>
