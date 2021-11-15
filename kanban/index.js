17921b767f9c4908d28a14888fcf2af94b2bf159
Jordan <jordanshackelford2@gmail.com>
2021-11-15 04:35:51 -0600

    can now add input new tasks for each category

--- kanban/index.js
+++ kanban/index.js
@@ -48,4 +48,14 @@ $(document).ready(function(){
         }
     });
 
+    //when #add button is clicked, create a new .task with the text in the input field
+    //then empty the input field
+    //only add the task to the .container that the button is clicked in
+    //keep the input and button in the same .container and at the bottom
+    $('.add').click(function(){
+        var input = $('#input').val();
+        var container = $(this).parent();
+        $(container).append('<div class="task">' + input + '</div>');
+        $('#input').val('');
+    });
 });
