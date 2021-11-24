07efc6fd79b9cb92bab77c0a5102c95e856ad900
Jordan <jordanshackelford2@gmail.com>
2021-11-23 19:44:50 -0600

    solved #22 in ruby

---
+++ leetcode/22. Generate Parentheses.rb
@@ -0,0 +1,23 @@
+# @param {Integer} n
+# @return {String[]}
+  
+def backtrack(result, current, open, close, max)
+    if current.length == max * 2
+        result << current
+        return
+    end
+    
+    if open < max
+        backtrack(result, current + '(', open + 1, close, max)
+    end
+    
+    if close < open
+        backtrack(result, current + ')', open, close + 1, max)
+    end
+end
+
+def generate_parenthesis(n)
+    result = []
+    backtrack(result, "", 0, 0, n)
+    result
+end
